aad94f2193f8171f5f97899e822f1f708c567d09
Lima2233 <islamriya493@gmail.com>
2024-05-15 11:02:11 +0600

    Add files via upload

---
+++ c# project limu.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE employees (
+
+id INT PRIMARY KEY IDENTITY(1,1),
+employee_id VARCHAR(MAX) NULL,
+full_name VARCHAR(MAX) NULL,
+gender VARCHAR(MAX) NULL,
+contact_number VARCHAR(MAX) NULL,
+position VARCHAR(MAX) NULL,
+salary INT NULL,
+insert_date DATE NULL,
+update_date DATE NULL,
+delete_date DATE NULL,
+status VARCHAR(MAX) NULL
+)
